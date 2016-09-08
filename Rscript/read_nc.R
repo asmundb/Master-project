2a16392ed9c504961a89c54e4eb138ce43d53f2d
Bakketun ]smund <aasmunba@torden.uio.no>
2016-09-08 14:08:17 +0200

    reads given variables from a list of files

---
+++ Rscript/read_nc.R
@@ -0,0 +1,32 @@
+# Read NetCDF files created bt SURFEX
+
+library("ncdf4")
+
+
+path  <- "/uio/hume/student-u88/aasmunba/Master-project/OUTPUT"
+
+files <- list.files(path=path, pattern="PREP_OFFLINE*",full.name=T) # test if nc maby
+files <- c(files, paste(path,"/PREP_SODA_EKF.nc",sep=""))
+nfiles<- length(files)
+
+vars <- c("TG1","TG2","WG1","WG2")
+nvars<- length(vars)
+                         #    npoints    
+                         #       v
+x    <- array(dim=c(nfiles,nvars,2))
+dimnames(x) <- list(letters[1:nfiles])
+
+for (i in 1:nfiles){
+  ncid    <- nc_open(files[i])
+  filename <- ncid$filename
+  filename <- gsub(".*/","",filename)
+  filename <- gsub("*.nc","",filename)
+  dimnames(x)[[1]][i] <- filename
+  for (j in 1:nvars){
+    x[i,j,]    <- ncvar_get(ncid,ncid$var[[vars[j]]])
+  }
+  nc_close(ncid)
+}
+dimnames(x)[[2]] <- vars
+
+
