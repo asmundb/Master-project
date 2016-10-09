ec1e619b1b53ff687da40ac7e4b923381ee30b73
aas-l <aas-l@aasl-X550VB>
2016-10-09 20:27:09 +0200

    yo

--- Rscript/plot_map.R
+++ Rscript/plot_map.R
@@ -4,7 +4,8 @@ library("fields")
 
 
 #map(plot=T, xlim=c(min(lon),max(lon)), ylim=c(min(lat),max(lat)))
-map(plot=T, xlim=c(-10,35), ylim=c(50,72))
+map(plot=T, xlim=c(90,180), ylim=c(-50,0),projection="cylindrical")
+
 
 map_axis = par("usr")
 
@@ -13,4 +14,8 @@ lat_range = which(map_axis[3] < lat & map_axis[4] > lat)
 
 par(new=T)
 #image.plot(lon,lat,Soil_moisture)
-image(lon[lon_range],lat[lat_range],Soil_moisture[lon_range, lat_range])
+image(     x=lon[lon_range],
+           y=lat[lat_range],
+           z=Soil_moisture[lon_range, lat_range])#,
+           xlim=c(min(lon[lon_range]),max(lon[lon_range]),
+           ylim=c(min(lat[lat_range]),max(lat[lat_range])))
