0224d11365a22f6a55261c01d3532a17df3fb8dd
Åsmund Bakketun <asmundb@157.249.118.18>
2016-11-11 12:19:48 +0100

    plot general timeseries

--- Rscript/read_nc.R
+++ Rscript/read_nc.R
@@ -11,8 +11,10 @@ source("functions.R")
 path  <- "/home/asmundb/SURFEX2/EXPERIMENTS/SODA_EKF/time_series"
 
 files.analysis  <- list.files(path=path, pattern="soda_analysis*",full.name=T) # test if nc mabye
+
 nfiles.analysis <- length(files.analysis)
 files.offline  <- list.files(path=path, pattern="soda_offline*",full.name=T)
+
 nfiles.offline  <- length(files.offline)
 
 
@@ -20,7 +22,7 @@ nfiles.offline  <- length(files.offline)
 stations <- read_stlist(stationlist="stationlist.cfg")
 
 #vars <- c("TG1","TG2","WG1","WG2")
-vars <- "TG1"
+vars <- "WG1"
 
 nvars <- length(vars)
 
@@ -38,29 +40,41 @@ for (i in 2:nfiles.offline){
 col = rainbow(8)
 
 #pdf("first_run_ever.pdf")
-plot(NA,xlim=c(0,24),ylim=c(min(var.offline),max(var.offline)))
+plot(NA,xlim=c(0,6*nfiles.offline),ylim=c(0,1))
 for (i in 1:8){
-  for (j in 1:4){
+  for (j in 1:nfiles.offline){
     lines(1:6+(j-1)*6, var.offline[1:6+(j-1)*6,i],col=col[i])
   }
-  lines(c(6,7), c(var.analysis[1,i], var.offline[7,i]),col=col[i])
-  lines(c(12,13), var.offline[12:13,i],col=col[i])
-  lines(c(18,19), c(var.analysis[2,i], var.offline[19,i]),col=col[i])
-  points(c(6,18),var.analysis[,i],col=col[i])
+# lines(c(6,7), c(var.analysis[1,i], var.offline[7,i]),col=col[i])
+# lines(c(12,13), var.offline[12:13,i],col=col[i])
+# lines(c(18,19), c(var.analysis[2,i], var.offline[19,i]),col=col[i])
+  points(seq(6,nfiles.analysis*12,12),var.analysis[,i],col=col[i])
 
 }
 
-obs1 <- read_OBSERVATION("~/SURFEX2/EXPERIMENTS/SODA_EKF/INPUT/OBSERVATIONS_161016H06.DAT")[[1]]
-obs2 <- read_OBSERVATION("~/SURFEX2/EXPERIMENTS/SODA_EKF/INPUT/OBSERVATIONS_161016H18.DAT")[[1]]
+#obs1 <- read_OBSERVATION("~/SURFEX2/EXPERIMENTS/SODA_EKF/INPUT/OBSERVATIONS_161016H06.DAT")[[1]]
+#obs2 <- read_OBSERVATION("~/SURFEX2/EXPERIMENTS/SODA_EKF/INPUT/OBSERVATIONS_161016H18.DAT")[[1]]
+
+obspath <- "/disk1/asmundb/SMOS/OBSERVATIONS"
+obsfiles <- list.files(path=obspath,pattern="OBSERVATIONS*", full.name=T)
+
+
+obs <- array(dim=c(0,8))
+for (i in 1:nfiles.analysis){
+  obs <- rbind(obs, read_OBSERVATION(obsfiles[i])[[1]])
+}
+
 
 #obs1[which(obs1==999)] <- -0.03
 #obs2[which(obs1==999)] <- -0.03
-points(rep(6,8), obs1, bg=col, pch=24)
-points(rep(18,8), obs2, bg=col, pch=24)
+for (i in 1:nfiles.analysis){
+  points(rep(12*(i-1)+6,8), obs[i,], bg=col, pch=24)
+}
+#points(rep(18,8), obs2, bg=col, pch=24)
 
-abline(v=c(6,18),lwd=0.1)
+#abline(v=c(6,18),lwd=0.1)
 
-legend("topleft", legend=dimnames(stations)[[1]], fill=col)
+#legend("topleft", legend=dimnames(stations)[[1]], fill=col)
 #legend("bottomleft", legend=c("SMOS", "analysis","offline"),
 
 #dev.off()
