0f880c5956ff46310c4cf9175a7c608dff6657d6
Bakketun ]smund <aasmunba@regnbue.uio.no>
2016-11-29 11:53:21 +0100

    extract smos files

--- Shellscript/SMOS_extract.sh
+++ Shellscript/SMOS_extract.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# download files using
-# wget -r ftp://c1e96d:xov8ng28@eftp.ifremer.fr/SM/GRIDDED/L3SM/RE04/MIR_CLF31A/2014/@DAY@
-
 # extract ncfiles from tgz files
 
 work="/disk1/asmundb/SMOS"
